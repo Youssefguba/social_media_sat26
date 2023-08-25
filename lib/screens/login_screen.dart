3d39f2b1a786067cb084df34e14bb9f003f559f3
Youssefguba <Youssef.guba@abwaab.me>
2023-08-26 00:32:06 +0300

    Initial commit

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,57 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+        children: [
+          Image.asset('assets/images/pattern.png'),
+          OutlinedButton(
+            onPressed: () async {
+              await signInWithGoogle();
+            },
+            child: ListTile(
+              title: const Text(
+                'Log in with Google',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              subtitle: const Text(
+                'Youssefguba',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              trailing: Image.asset('assets/images/logo_google.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<UserCredential> signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser =
+        await GoogleSignIn(scopes: ['profile', 'email']).signIn();
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+}
